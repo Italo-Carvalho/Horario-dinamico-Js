6d81e0b74d246589a9d19fc7baaf56eb887980e5
Ulises Viña <playerlgam@live.com>
2020-10-22 21:59:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function carregar() {
     document.getElementById("msg").innerHTML = `Agora são ${hora} horas.`
     switch(true) {
-            case(data.getHours() >= 8 && data.getHours() < 12) {
+            case(data.getHours() >= 8 && data.getHours() < 12):
                 img.src = "manha.png";
                 document.body.style.background = "#00b8b4";
-            }
-            case(data.getHours() >= 12 && data.getHours() <= 18) {
+            break;
+            case(data.getHours() >= 12 && data.getHours() <= 18):
                 img.src = "tarde.png"
                 document.body.style.background = "#fcaf51"
-            }
-            case(data.getHours() >= 18 && data.getHours() < 7) {
+            break;
+            case(data.getHours() >= 18 && data.getHours() < 7):
                 img.src = 'noite.png'
                 document.body.style.background = '#001f4d'
-            }
+            break;
     }
 }
